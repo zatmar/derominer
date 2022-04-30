4092460614369a523af96b949bbf94adda1c86ef
Zatmar <11079007+zatmar@users.noreply.github.com>
2022-04-30 03:17:35 +0200

    clean

--- astrobwt/astrobwt_fast/endian_big.go
+++
@@ -1,9 +0,0 @@
-//go:build armbe || arm64be || mips || mips64 || ppc64 || s390 || s390x || sparc || sparc64
-// +build armbe arm64be mips mips64 ppc64 s390 s390x sparc sparc64
-
-package astrobwt_fast
-
-const LittleEndian = false
-const BigEndian = true
-
-// see https://github.com/golang/go/blob/master/src/go/build/syslist.go
